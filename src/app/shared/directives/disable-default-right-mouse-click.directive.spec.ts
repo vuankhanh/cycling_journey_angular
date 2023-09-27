3a21f91d9c41db089b9a0eb198d845ae418da35a
VU AN KHANH <vuankhanh19071992@gmail.com>
2023-09-28 01:00:01 +0700

    first commit

--- src/app/shared/directives/disable-default-right-mouse-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisableDefaultRightMouseClickDirective } from './disable-default-right-mouse-click.directive';
-
-describe('DisableDefaultRightMouseClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisableDefaultRightMouseClickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
