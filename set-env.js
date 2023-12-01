b869765d6608c2c94e92992e4c37287deea5bad7
VU AN KHANH <vuankhanh19071992@gmail.com>
2023-12-01 14:57:17 +0700

    update yml

--- set-env.js
+++ set-env.js
@@ -1,12 +1,10 @@
-require('dotenv').config();
 const fs = require('fs');
-const writeFileSync = fs.writeFileSync;
 // Configure Angular `environment.ts` file path
 const targetPath = './src/environments/environment.staging.ts';
 // `environment.development.ts` file structure
 const backendApi = process.env.STAGING_BE_API;
 const backendStatic = process.env.STAGING_BE_STATIC;
-const googleMapApiKey = process.env.GOOGLE_MAP_API_KEY;
+const googleMapApiKey = process.env.STAGINGDEV_GOOGLE_MAP_API_KEY;
 
 const environment = {
     backendApi,
@@ -16,7 +14,7 @@ const environment = {
 
 const envConfigFile = `export const environment = ${JSON.stringify(environment)};`;
 const setEnv = () => {
-    writeFileSync(targetPath, envConfigFile, { encoding: "utf8" });
+    fs.writeFileSync(targetPath, envConfigFile, { encoding: "utf8" });
 };
 
 setEnv();
